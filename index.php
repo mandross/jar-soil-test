c4519030fea4fd0c66c24e6ee791191c31a00619
mandross <kuba@slashdev.team>
2025-07-31 06:51:41 +0200

    leave the svg untouched

--- index.php
+++ index.php
@@ -128,6 +128,7 @@ if (empty($errors)) {
 </svg> -->
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="95%" height="95%" viewBox="-219 -20 440 420">
  <title>SoilTexture USDA</title>
+ <desc>A soil texture diagram redrawn from the USDA webpage http://nrcs.usda.gov/Internet/FSE_MEDIA/nrcs142p2_050242.jpg with colours from http://commons.wikimedia.org/wiki/File:SoilTexture_USDA.png by CMG Lee.</desc>
  <style type="text/css">
 #main  { font-size:16px; font-family:Helvetica,Arial,sans-serif; text-anchor:middle;
          fill:#000000; stroke-linejoin:round; }
@@ -173,46 +174,49 @@ text   { stroke:none; cursor:default; }
  <circle cx="0" cy="0" r="99999" fill="#ffffff"/>
  <g id="main" transform="scale(1,0.866)">
   <g stroke="#000000" stroke-width="1">
-   <g class="clay thick">
-     <text transform="translate(100,210) scale(0.866,1)" y="0.6ex"><?php echo t('svg_clay_line'); ?></text>
+   <switch>
+    <g class="clay thick" systemLanguage="aa">
+     <text transform="translate(100,210) scale(0.866,1)" y="0.6ex">50% clay line</text>
      <use xlink:href="#line_clay"/>
-   </g>
-   <g class="silt thick">
-     <text transform="translate(-120,400) scale(0.866,1) rotate(-60)" y="-0.6ex"><?php echo t('svg_silt_line'); ?></text>
+    </g>
+    <g class="silt thick" systemLanguage="ba">
+     <text transform="translate(-120,400) scale(0.866,1) rotate(-60)" y="-0.6ex">20% silt line</text>
      <use xlink:href="#line_silt"/>
      <use xlink:href="#line_clay"/>
-   </g>
-   <g class="sand thick">
-     <text transform="translate( 80,400) scale(0.866,1) rotate(60)" y="-0.6ex" text-anchor="end"><?php echo t('svg_sand_line'); ?></text>
+    </g>
+    <g class="sand thick" systemLanguage="ca">
+     <text transform="translate( 80,400) scale(0.866,1) rotate(60)" y="-0.6ex" text-anchor="end">30% sand line</text>
      <use xlink:href="#line_sand"/>
      <use xlink:href="#line_silt"/>
      <use xlink:href="#line_clay"/>
-   </g>
+     <use xlink:href="#sample1"/>
+    </g>
     <g systemLanguage="da">
      <use xlink:href="#sample1"/>
      <use xlink:href="#sample2"/>
      <use xlink:href="#sample3"/>
     </g>
-   <g>
-     <path d="M 0,0 l 80,160 l -40,80 h -100 l -30,-60 z" fill="#ffff9c"/><text transform="translate(0,150)" y="0.6ex"><tspan><?php echo t('svg_clay'); ?></tspan></text>
-     <path d="M -60,240 h 100 l 25,50 h -100 z" fill="#ceff63"/><text transform="translate(5,270)" y="0.6ex"><tspan><?php echo t('svg_clay_loam'); ?></tspan></text>
-     <path d="M -35,290 h 90 l -40,80 h -40 l -25,-50 z" fill="#ce9c00"/><text transform="translate(-5,330)" y="0.6ex"><tspan><?php echo t('svg_loam'); ?></tspan></text>
-     <path d="M -90,180 l 40,80 h -80 z" fill="#ff0000"/><text transform="translate(-90,230)" y="0.6ex"><tspan><?php echo t('svg_sandy'); ?></tspan><tspan x="0" dy="20"><?php echo t('xys'); ?></tspan></text>
-     <path d="M -130,260 h 80 l 15,30 l -15,30 h -110 z" fill="#ff9c9c"/><text transform="translate(-90,290)" y="0.6ex"><tspan><?php echo t('svg_sandy_clay'); ?></tspan><tspan x="0" dy="20"><?php echo t('svg_loam'); ?></tspan></text>
-     <path d="M -160,320 h 110 l 25,50 h 40 l -15,30 h -80 l -90,-60 z" fill="#ffceff"/><text transform="translate(-90,350)" y="0.6ex"><tspan><?php echo t('svg_sandy_loam'); ?></tspan></text>
-     <path d="M -170,340 l 90,60 h -60 l -40,-40 z" fill="#ffcece"/><text transform="translate(-142,375) scale(0.95,1)" y="0.6ex"><tspan><?php echo t('svg_loamy_sand'); ?></tspan></text>
-     <path d="M -180,360 l 40,40 h -60 z" fill="#ffce9c"/><text transform="translate(-175,390)" y="0.6ex"><tspan><?php echo t('svg_sand'); ?></tspan></text>
-     <path d="M 80,160 l 40,80 h -80 z" fill="#9cffce"/><text transform="translate(80,210)" y="0.6ex"><tspan><?php echo t('svg_silty_clay'); ?></tspan></text>
-     <path d="M 40,240 h 80 l 25,50 h -80 z" fill="#63ce9c"/><text transform="translate(90,250)" y="0.6ex"><tspan><?php echo t('svg_silty_clay_loam'); ?></tspan></text>
-    <path d="M 55,290 h 90 l 30,60 h -30 l -25,50 h -120 z" fill="#9cce00"/><text transform="translate(90,350)" y="0.6ex"><tspan><?php echo t('svg_silt_loam'); ?></tspan></text>
-    <path d="M 145,350 h 30 l 25,50 h -80 z" fill="#00ff31"/><text transform="translate(160,370)" y="0.6ex"><tspan><?php echo t('svg_silt'); ?></tspan></text>
-   </g>
+    <g>
+     <path d="M 0,0 l 80,160 l -40,80 h -100 l -30,-60 z" fill="#ffff9c"/><text transform="translate(0,150)" y="0.6ex"><tspan>clay</tspan></text>
+     <path d="M -60,240 h 100 l 25,50 h -100 z" fill="#ceff63"/><text transform="translate(5,270)" y="0.6ex"><tspan>clay loam</tspan></text>
+     <path d="M -35,290 h 90 l -40,80 h -40 l -25,-50 z" fill="#ce9c00"/><text transform="translate(-5,330)" y="0.6ex"><tspan>loam</tspan></text>
+     <path d="M -90,180 l 40,80 h -80 z" fill="#ff0000"/><text transform="translate(-90,230)" y="0.6ex"><tspan>sandy</tspan><tspan x="0" dy="20">clay</tspan></text>
+     <path d="M -130,260 h 80 l 15,30 l -15,30 h -110 z" fill="#ff9c9c"/><text transform="translate(-90,290)" y="0.6ex"><tspan>sandy clay</tspan><tspan x="0" dy="20">loam</tspan></text>
+     <path d="M -160,320 h 110 l 25,50 h 40 l -15,30 h -80 l -90,-60 z" fill="#ffceff"/><text transform="translate(-90,350)" y="0.6ex"><tspan>sandy loam</tspan></text>
+     <path d="M -170,340 l 90,60 h -60 l -40,-40 z" fill="#ffcece"/><text transform="translate(-142,375) scale(0.95,1)" y="0.6ex"><tspan>loamy</tspan><tspan x="13" dy="13">sand</tspan></text>
+     <path d="M -180,360 l 40,40 h -60 z" fill="#ffce9c"/><text transform="translate(-175,390)" y="0.6ex"><tspan>sand</tspan></text>
+     <path d="M 80,160 l 40,80 h -80 z" fill="#9cffce"/><text transform="translate(80,210)" y="0.6ex"><tspan>silty</tspan><tspan x="0" dy="20">clay</tspan></text>
+     <path d="M 40,240 h 80 l 25,50 h -80 z" fill="#63ce9c"/><text transform="translate(90,250)" y="0.6ex"><tspan>silty</tspan><tspan x="5" dy="20">clay loam</tspan></text>
+     <path d="M 55,290 h 90 l 30,60 h -30 l -25,50 h -120 z" fill="#9cce00"/><text transform="translate(90,350)" y="0.6ex"><tspan>silt loam</tspan></text>
+     <path d="M 145,350 h 30 l 25,50 h -80 z" fill="#00ff31"/><text transform="translate(160,370)" y="0.6ex"><tspan>silt</tspan></text>
+    </g>
+   </switch>
+   <path d="M 0,0 l 200,400 h -400 Z" fill="url(#pattern_grid)"/>
   </g>
-  <path d="M 0,0 l 200,400 h -400 Z" fill="url(#pattern_grid)"/>
   <g class="clay axis">
    <g class="arrow" transform="translate(-100,200) scale(0.866,1) rotate(-60)">
     <use xlink:href="#arrow"/>
-    <text y="0.6ex" dy="-50"><?php echo t('svg_axis_clay'); ?></text>
+    <text y="0.6ex" dy="-50">Clay Separate (%)</text>
    </g>
    <text transform="translate(   0,  0) scale(0.866,1)" y="0.5ex">100&#8202;-</text>
    <text transform="translate( -20, 40) scale(0.866,1)" y="0.5ex" >90&#8202;-</text>
@@ -229,7 +233,7 @@ text   { stroke:none; cursor:default; }
   <g class="silt axis">
    <g class="arrow" transform="translate(100,200) scale(0.866,1) rotate(60)">
     <use xlink:href="#arrow"/>
-    <text y="0.6ex" dy="-50"><?php echo t('svg_axis_silt'); ?></text>
+    <text y="0.6ex" dy="-50">Silt Separate (%)</text>
    </g>
    <text transform="translate(  0,  0) scale(0.866,1) rotate(-60)" y="0.5ex">-&#8201;0</text>
    <text transform="translate( 20, 40) scale(0.866,1) rotate(-60)" y="0.5ex">-&#8202;10</text>
@@ -246,7 +250,7 @@ text   { stroke:none; cursor:default; }
   <g class="sand axis">
    <g class="arrow" transform="translate(0,400) scale(0.866,1) rotate(180)">
     <use xlink:href="#arrow"/>
-    <text transform="rotate(180)" y="0.6ex" dy="50"><?php echo t('svg_axis_sand'); ?></text>
+    <text transform="rotate(180)" y="0.6ex" dy="50">Sand Separate (%)</text>
    </g>
    <text transform="translate(-200,400) scale(0.866,1) rotate(60)" y="0.5ex">-&#8202;100</text>
    <text transform="translate(-160,400) scale(0.866,1) rotate(60)" y="0.5ex">-&#8201;90</text>
